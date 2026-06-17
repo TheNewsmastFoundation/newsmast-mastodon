79dabff0532d90062909dcc4d31bc633ca188bbb
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2026-06-17 13:52:45 +0630

    Add missing locales file

--- app/mailers/custom_passwords_mailer.rb
+++ app/mailers/custom_passwords_mailer.rb
@@ -2,7 +2,7 @@
 
 # Source: accounts/app/mailers/custom_passwords_mailer.rb
 class CustomPasswordsMailer < ApplicationMailer
-  layout 'email'
+  layout "email"
 
   helper BrandColorHelper
   helper LogoHelper
@@ -10,10 +10,10 @@ class CustomPasswordsMailer < ApplicationMailer
   def reset_password_confirmation
     @user = params[:user]
 
-    sender_name = ENV.fetch('MAIL_SENDER_NAME', 'Development Patchwork')
+    sender_name = ENV.fetch("MAIL_SENDER_NAME", "Development Patchwork")
 
     if @user.present?
-      @subject = 'OTP verification code'
+      @subject = "OTP verification code"
       mail(
         to: @user.email,
         subject: @subject
