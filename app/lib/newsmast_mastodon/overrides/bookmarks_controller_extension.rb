6c30a78d32853a5974a6cc925cb9ddd1bab97ea6
Nyan Lin Htut <nyanlinhtut925@gmail.com>
2026-06-25 15:24:26 +0700

    refactor: unify response decoration by replacing specific methods with a common decorate_statuses_response! method

--- app/lib/newsmast_mastodon/overrides/bookmarks_controller_extension.rb
+++ app/lib/newsmast_mastodon/overrides/bookmarks_controller_extension.rb
@@ -7,33 +7,7 @@ module NewsmastMastodon
 
       def index
         super
-        decorate_bookmarks_response!
-      end
-
-      private
-
-      def decorate_bookmarks_response!
-        payload = json_payload_array(response.body)
-        return if payload.empty?
-
-        status_ids = payload.filter_map { |status| status['id']&.to_i }
-        return if status_ids.empty?
-
-        reactions_map = build_patchwork_post_reactions(Status.where(id: status_ids).select(:id).to_a)
-
-        payload.each do |status|
-          status_id = status['id']&.to_i
-          status['patchwork_post_reactions'] = reactions_map[status_id] || []
-        end
-
-        self.response_body = JSON.generate(payload)
-      end
-
-      def json_payload_array(body)
-        parsed = JSON.parse(body)
-        parsed.is_a?(Array) ? parsed : []
-      rescue JSON::ParserError
-        []
+        decorate_statuses_response!
       end
     end
   end
--- app/lib/newsmast_mastodon/overrides/timeline_patchwork_post_reactions.rb
+++ app/lib/newsmast_mastodon/overrides/timeline_patchwork_post_reactions.rb
@@ -5,6 +5,30 @@ module NewsmastMastodon
     module TimelinePatchworkPostReactions
       private
 
+      def decorate_statuses_response!
+        payload = json_payload_array(response.body)
+        return if payload.empty?
+
+        status_ids = payload.filter_map { |status| status['id']&.to_i }
+        return if status_ids.empty?
+
+        reactions_map = build_patchwork_post_reactions(Status.where(id: status_ids).select(:id).to_a)
+
+        payload.each do |status|
+          status_id = status['id']&.to_i
+          status['patchwork_post_reactions'] = reactions_map[status_id] || []
+        end
+
+        self.response_body = JSON.generate(payload)
+      end
+
+      def json_payload_array(body)
+        parsed = JSON.parse(body)
+        parsed.is_a?(Array) ? parsed : []
+      rescue JSON::ParserError
+        []
+      end
+
       def build_patchwork_post_reactions(statuses, account: current_account)
         status_ids = statuses.map(&:id)
         return {} if status_ids.empty?
